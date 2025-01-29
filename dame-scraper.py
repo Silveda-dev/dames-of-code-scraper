f21b367b11e2024831e24651f0329527dbbe7b95
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2025-01-29 19:20:27 +1030

    Switch to function and loop functionality for better scaling

--- dame-scraper.py
+++ dame-scraper.py
@@ -6,24 +6,33 @@ dames = ["Ada Lovelace", "Grace Hopper", "Radia Perlman", "Betty Snyder", "Betty
 dame_links = ["https://en.wikipedia.org/wiki/Ada_Lovelace", "https://en.wikipedia.org/wiki/Grace_Hopper", "https://en.wikipedia.org/wiki/Radia_Perlman",
               "https://en.wikipedia.org/wiki/Betty_Holberton", "https://en.wikipedia.org/wiki/Jean_Bartik"]
 
+def dame_finder():
+    #Generating dame of focus
+    i = random.randint(0, (len(dames)-1)) 
+    print(dames[i], "\n\n")
+
+    #Scraping Wikipedia page of dame
+    url = dame_links[i]
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    refs = soup.find_all('sup')
+    for ref in refs:
+        ref.decompose()
+
+    info = soup.find_all('p')
+    for x in info:
+        print(x.text.strip())
+        print()
+
 print("Welcome to Dames of Code - recognising historic (and often forgotten) women in tech.\n\n")
 
-#Generating dame of focus
-i = random.randint(0, (len(dames)-1))
-print(dames[i], "\n\n")
+cont = True
 
-#Scraping Wikipedia page of dame
-url = dame_links[i]
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-
-refs = soup.find_all('sup')
-for ref in refs:
-    ref.decompose()
-
-info = soup.find_all('p')
-for x in info:
-    print(x.text.strip())
-    print()
+while cont:
+    dame_finder()
+    check = input("Do you wish to find out about another dame (y/n)? ")
+    if check == 'n' or check == 'N' or check == 'no' or check == 'No':
+        cont = False
 
 print("Thank you for educating yourself.")
